5dffdfb327503082451c3d9ea3b35cbd148a96ee
Boris Yurkevich <boris@cocoaswitch.com>
2020-11-21 22:35:56 +0000

    Initial Commit

---
+++ Thinking in SwiftUI Solutions/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Thinking in SwiftUI Solutions
+//
+//  Created by Boris Yurkevich on 21/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Thinking in SwiftUI Solutions/Thinking_in_SwiftUI_SolutionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Thinking_in_SwiftUI_SolutionsApp.swift
+//  Thinking in SwiftUI Solutions
+//
+//  Created by Boris Yurkevich on 21/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct Thinking_in_SwiftUI_SolutionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
