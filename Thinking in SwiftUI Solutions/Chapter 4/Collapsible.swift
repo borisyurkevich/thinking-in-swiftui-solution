bb11eafc50bc4b53efa23f2f1a9911f5411cbd27
Boris Yurkevich <boris@cocoaswitch.com>
2020-11-27 04:35:19 +0000

    Chapter 4
    Improve my solution by adding colour

--- Thinking in SwiftUI Solutions/Chapter 4/Collapsible.swift
+++ Thinking in SwiftUI Solutions/Chapter 4/Collapsible.swift
@@ -22,10 +22,9 @@ struct Collapsible<Element, Content: View>: View {
 
     var body: some View {
         HStack(alignment: alignment, spacing: expanded ? spacing : 0) {
-            ForEach(data.indices,
-                    content:{
-                        child(at: $0)
-                    })
+            ForEach(data.indices) { idx in
+                child(at: idx)
+            }
         }
         .border(Color.black)
     }
@@ -42,13 +41,20 @@ struct Collapsible<Element, Content: View>: View {
 
 struct Collapsible_Previews: PreviewProvider {
     
-  static var previews: some View {
+    static var previews: some View {
+    
+    let data: [(CGFloat, Color)] = [(100, .green), (50, .yellow), (200, .blue)]
+    
     Group {
-        Collapsible(alignment: .bottom, expanded: true, spacing: 4, data: [100, 50, 200], content: { size in
-        Rectangle().frame(width: size, height: size)
+        Collapsible(alignment: .bottom, expanded: true, spacing: 4, data: data, content: { item in
+            Rectangle()
+                .fill(item.1)
+                .frame(width: item.0, height: item.0)
       })
-        Collapsible(expanded: false, data: [100, 50, 200], content: { width in
-        Rectangle().frame(width: width, height: width)
+        Collapsible(expanded: false, data: data, content: { item in
+            Rectangle()
+                .fill(item.1)
+                .frame(width: item.0, height: item.0)
       })
     }.previewLayout(.sizeThatFits) // This removes device bezel
   }
